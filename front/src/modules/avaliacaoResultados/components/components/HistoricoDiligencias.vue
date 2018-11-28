833c6c3e443f1990b355c0cd1c2156085ad1a284
leonardo <volthier@gmail.com>
2018-11-28 15:31:26 -0200

    [UPDATE] Layout do Historico  Diligencias

--- front/src/modules/avaliacaoResultados/components/components/HistoricoDiligencias.vue
+++ front/src/modules/avaliacaoResultados/components/components/HistoricoDiligencias.vue
@@ -1,7 +1,8 @@
 <template>
         <v-dialog v-model="dialog"
+
                   scrollable
-                  fullscreen
+                  max-width="850px"
         >
             <v-tooltip slot="activator" bottom>
                 <v-btn slot="activator" flat icon @click.native="obterDiligencias(obj.idPronac);">
@@ -12,7 +13,7 @@
 
             <v-card>
 
-                <v-toolbar dark color="green">
+                <v-toolbar dark color="#0a420e !important">
                     <v-btn icon dark @click.native="dialog = false">
                         <v-icon>close</v-icon>
                     </v-btn>
@@ -21,33 +22,22 @@
 
                 <v-divider></v-divider>
                 <v-card-text>
-                    <v-timeline >
-                        <v-timeline-item
+                        <v-flex
                             id="time"
                             v-for="(item, i) in sortByDate"
-                            :key="i"
-                            small
-                        >
-                             <span
-                                 slot="opposite"
-                                 :class="`headline font-weight-bold green--text`"
-                             >
-                                 Solicitado: {{item.dataSolicitacao | date}} <br/>
-                                 <span v-if="item.dataResposta">  Respondido: {{item.dataResposta | date}}</span>
-                             </span>
-
+                            :key="i">
                             <v-card color="green">
                                 <v-card-title dark class="title white--text">{{item.tipoDiligencia}} <span v-if="item.stProrrogacao"> - {{item.stProrrogacao}}</span></v-card-title>
                                 <v-card-text class="white text--primary">
                                      <v-expansion-panel>
                                         <v-expansion-panel-content v-if="item.Solicitacao">
-                                            <div slot="header">Solicitação</div>
+                                            <div slot="header">Solicitação: {{item.dataSolicitacao | date}}</div>
                                             <v-card>
                                                 <v-card-text v-html="item.Solicitacao"></v-card-text>
                                             </v-card>
                                         </v-expansion-panel-content>
                                         <v-expansion-panel-content v-if="item.Resposta">
-                                            <div slot="header" class="font-weight-regular">Resposta</div>
+                                            <div slot="header" class="font-weight-regular">Resposta: : {{item.dataResposta | date}}</div>
                                             <v-card>
                                                 <v-card-text v-html="item.Resposta"></v-card-text>
                                             </v-card>
@@ -55,9 +45,7 @@
                                     </v-expansion-panel>
                                 </v-card-text>
                             </v-card>
-
-                        </v-timeline-item>
-                    </v-timeline>
+                        </v-flex>
                 </v-card-text>
             </v-card>
         </v-dialog>

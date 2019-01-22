89bf19b74d2844030fc0693e07b7a98005b86fa6
Cleber Santos <oclebersantos@gmail.com>
2019-01-22 00:05:59 -0200

    [refactor]  proposta: custos vinculados para vuetify #1878

--- front/src/modules/proposta/visualizar/components/PropostaCustosVinculados.vue
+++ front/src/modules/proposta/visualizar/components/PropostaCustosVinculados.vue
@@ -1,18 +1,34 @@
 <template>
-    <div class="tabelas">
-        <div class="row">
-            <slTabelaSimples :dados="dados"/>
-        </div>
-    </div>
+    <v-card
+        class="mb-2">
+        <v-card-title
+            primary
+            class="title">Custos vinculados</v-card-title>
+        <v-data-table
+            :headers="headers"
+            :items="dados.lines"
+            class="elevation-1"
+        >
+            <template
+                slot="items"
+                slot-scope="props">
+                <td>{{ props.item.item }}</td>
+                <td>{{ props.item.dtCadastro | formatarData }}</td>
+                <td>{{ props.item.pcCalculo }}</td>
+            </template>
+        </v-data-table>
+    </v-card>
 </template>
 <script>
 import slTabelaSimples from '@/components/slTabelaSimples';
+import { utils } from '@/mixins/utils';
 
 export default {
     name: 'PropostaCustosVinculados',
     components: {
         slTabelaSimples,
     },
+    mixins: [utils],
     props: {
         arrayCustos: {
             type: Object,
@@ -22,6 +38,11 @@ export default {
     data() {
         return {
             dados: [],
+            headers: [
+                { text: 'Item', value: 'item' },
+                { text: 'Data', value: 'dtCadastro' },
+                { text: 'Percentual', value: 'pcCalculo' },
+            ],
         };
     },
     watch: {

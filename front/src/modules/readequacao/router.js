a611198705e9f060b844ffebbe68c3eca18055f1
fernao <fernao@riseup.net>
2019-04-03 15:11:35 -0300

    Remove rota de saldo

--- front/src/modules/readequacao/router.js
+++ front/src/modules/readequacao/router.js
@@ -2,14 +2,6 @@ import SaldoAplicacao from './views/SaldoAplicacaoView';
 import PainelReadequacoes from './views/PainelReadequacoesView';
 
 export default [
-    /*
-    {
-        path: '/readequacao/saldo-aplicacao/:idPronac',
-        component: SaldoAplicacao,
-        meta: {
-            title: 'Saldo de aplicação',
-        },
-    },*/
     {
         path: '/readequacao/painel/:idPronac',
         name: 'PainelReadequacoes',

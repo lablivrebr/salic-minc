68449e331eb03744f2bc5b761e543dc615c6bb63
leonardo <volthier@gmail.com>
2019-04-11 16:01:05 -0300

    [UPDATE] Estrutura controller Model e Service #2040

---
+++ application/modules/movimentacao-bancaria/controllers/TransferenciaRecursoController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: leonardo
+ * Date: 11/04/19
+ * Time: 14:10
+ */
+
+namespace Application\Modules\MovimentacaoBancaria\controllers;
+
+
+class TransferenciaRecursoController
+{
+
+}
---
+++ application/modules/movimentacao-bancaria/service/Captacao.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: leonardo
+ * Date: 11/04/19
+ * Time: 11:38
+ */
+
+namespace Application\Modules\MovimentacaoBancaria\service;
+
+
+class Captacao
+{
+
+}

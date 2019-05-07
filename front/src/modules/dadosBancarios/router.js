3895bcef0627ed0c310a9b3f8ea2e470d103c57a
leonardo <volthier@gmail.com>
2019-05-07 18:54:27 -0300

    [UPDATE] Templates de esboço inicial issue #2043

--- front/src/modules/dadosBancarios/router.js
+++ front/src/modules/dadosBancarios/router.js
@@ -1,17 +1,29 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Captacao from './components/captacao/registro';
+import Registrar from './components/captacao/Registrar';
+import TabelaVizualizacao from './components/captacao/TabelaVizualizacao';
+import Vizualizar from './components/captacao/Vizualizar';
 
 Vue.use(Router);
 
 const routes = [
     {
         path: '/captacao-recurso',
-        name: 'Captacao',
-        component: Captacao,
+        name: 'TabelaVizualizacao',
+        component: TabelaVizualizacao,
         meta: {
             title: 'Dados Bancario Captação Recursos',
         },
+    },
+    {
+        path: '/captacao-recurso/registrar',
+        name: 'Registrar',
+        component: Registrar,
+    },
+    {
+        path: '/captacao-recurso/vizualizar',
+        name: 'Vizualizar',
+        component: Vizualizar,
     },
 ];
 
